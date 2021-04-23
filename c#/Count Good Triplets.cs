0c548c3e8901687419e3e8c2eea5fd6842845fa2
Aleksandr Pilipenko <56961085+PilipenkoAB@users.noreply.github.com>
2021-04-23 19:19:25 +1200

    Add files via upload

---
+++ c#/Count Good Triplets.cs
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/count-good-triplets/
+public class Solution {
+    public int CountGoodTriplets(int[] arr, int a, int b, int c) {
+        int res = 0;
+        for (int i = 0; i < arr.Length-2; i++){
+            for (int j = i+1; j < arr.Length-1; j++){
+                for (int k = j+1; k < arr.Length; k++){
+                    if(Math.Abs(arr[i] - arr[j]) <= a && Math.Abs(arr[j] - arr[k]) <= b && Math.Abs(arr[i] - arr[k]) <= c){
+                       res ++; 
+                    }
+                }   
+            }
+        }
+        return res;
+    }
+}
