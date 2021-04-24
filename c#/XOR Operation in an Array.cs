2c9900eaa38517753e9b6eeedb8876d2c60862d7
Aleksandr Pilipenko <56961085+PilipenkoAB@users.noreply.github.com>
2021-04-24 21:57:09 +1200

    Add files via upload

---
+++ c#/XOR Operation in an Array.cs
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/xor-operation-in-an-array/
+public class Solution {
+    public int XorOperation(int n, int start) {
+        
+        int res = start;
+        for(int i = 1; i < n; i++){
+            res = res ^ (start + 2*i);
+        }  
+        return res;
+    }
+}
